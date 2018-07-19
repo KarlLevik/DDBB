0f7ace82882addb09bbeca2e8a4823afcf3901bd
Milan Tancak <mtancak1@sheffield.ac.uk>
2018-07-19 15:06:11 +0100

    Deleted duplicate 0s in both alphabets

--- DDBB/MongoDriver.java
+++ DDBB/MongoDriver.java
@@ -224,7 +224,7 @@ public class MongoDriver {
 	public static String generateRandomString(Integer length, Boolean use_plain){		
 		Random r = new Random();
 		String rng_val = "";
-		String alphabet = use_plain ? "01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" : "01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`,./;'#[]-=¬!£$%^&*()<>?:@~{}_+";
+		String alphabet = use_plain ? "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" : "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`,./;'#[]-=¬!£$%^&*()<>?:@~{}_+";
 		while(rng_val.length() != length){
 			rng_val = rng_val + alphabet.charAt(r.nextInt(alphabet.length()));
 		}
