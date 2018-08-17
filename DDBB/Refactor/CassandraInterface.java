547d73a9f1549b2b7f0d85810db194dc6e0d63b0
mtancak1 <tancak.milan@gmail.com>
2018-08-17 11:54:47 +0100

    Read operations finished

--- DDBB/Refactor/CassandraInterface.java
+++ DDBB/Refactor/CassandraInterface.java
@@ -31,6 +31,9 @@ public class CassandraInterface implements Db {
 
     public void disconnectDb(){
 
+        session.close();
+        cluster.close();
+
     }
 
     public void table(){
@@ -159,9 +162,124 @@ public class CassandraInterface implements Db {
 
     public long read(Hashtable<String,ArrayList<Object>> in){
 
-        session.execute("select * from myKeyspace.myTable where id = 1").one();
+        StringBuilder sb = new StringBuilder("SELECT * from " + cfg.settings.get("db_name") + "." + cfg.settings.get("table") + " where ");
 
-        return Long.parseLong("0");
+        Iterator<String> it = in.keySet().iterator();
+        String last_key = "";
+        while(it.hasNext()){
+            last_key = it.next();
+        }
+
+        int i = 0;
+        for(String key : in.keySet()){
+
+            sb.append(key + " = ");
+
+
+
+
+
+
+
+
+
+
+
+            int cfg_field_list_index = cfg.read.data.get("name").indexOf(key);
+
+            if(in.get(key).size() == 1 && (boolean) cfg.read.data.get("length_up_to").get(cfg_field_list_index)){
+
+                System.out.println("2");
+
+                if(cfg.read.data.get("val").get(cfg_field_list_index).equals("STRING")){
+                    sb.append("'");
+
+                    System.out.println("3");
+                }
+                System.out.println("out = " + in.get(key));
+                sb.append(String.valueOf(in.get(key).get(0)));
+                if(cfg.read.data.get("val").get(cfg_field_list_index).equals("STRING")){
+                    sb.append("'");
+                }
+
+                if(i != in.keySet().size() - 1){
+                    sb.append(", ");
+                }
+            } else if(in.get(key).size() > 0){
+                sb.append("[");
+                for(int v = 0; v < in.get(key).size(); v++){
+
+                    if(in.get(key).get(v) != null && !in.get(key).get(v).equals("null") && v != in.get(key).size() - 1){
+                        if(cfg.read.data.get("val").get(cfg_field_list_index).equals("STRING")){
+
+                            sb.append("'");
+                            sb.append(String.valueOf(in.get(key).get(v)));
+                            sb.append("', ");
+
+                        } else {
+                            sb.append(String.valueOf(in.get(key).get(v)));
+                            sb.append(", ");
+                        }
+
+                    } else if(in.get(key).get(v) != null && !in.get(key).get(v).equals("null")){
+                        if(cfg.read.data.get("val").get(cfg_field_list_index).equals("STRING")){
+
+                            sb.append("'");
+                            sb.append(String.valueOf(in.get(key).get(v)));
+                            sb.append("'");
+
+                        } else {
+                            sb.append(String.valueOf(in.get(key).get(v)));
+                        }
+                    } else {
+                        if(cfg.read.data.get("val").get(cfg_field_list_index).equals("STRING")){
+
+                            sb.append("''");
+
+                        }
+                    }
+
+                }
+
+                sb.append("]");
+
+                if(i != in.keySet().size() - 1){
+                    sb.append(", ");
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            if(in.keySet().size() > 1 && i < in.keySet().size() - 1){
+                sb.append(", ");
+            }
+
+        }
+
+        if(i == in.keySet().size() - 1 && cfg.read.meta.containsKey("allow_filtering")){
+            if((boolean) cfg.read.meta.get("allow_filtering")) {
+                sb.append(" ALLOW FILTERING");
+            }
+        }
+
+        sb.append(";");
+
+        String query = sb.toString();
+        System.out.println("query = " + query);
+        Long time_before = System.nanoTime();
+        session.execute(query);
+        Long time_after = System.nanoTime();
+
+        return time_after - time_before;
 
     }
 
